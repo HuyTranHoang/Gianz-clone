2242b7b917c340acb9a637a2cf9aa438b70231c9
Sosovi <nhokpio95@gmail.com>
2022-03-27 00:40:20 +0700

    js

--- assets/js/tsd_script.js
+++ assets/js/tsd_script.js
@@ -2,33 +2,28 @@
 
 $(function(){
 
-  $("#previewForm").load("./template-1.html"); 
+  $("#previewForm").load("./template-1.html");
+  
+  var idName,value,element;
   
   $("input").keyup(function() {
-    var idName = $(this).data("for"); 
-    var value = $(this).val();
+    idName = $(this).data("for"); 
+    value = $(this).val();
     $("#" + idName).html(value);
   });
   
-
   $("input").focus(function() {
-    var idName = $(this).data("for"); 
+    idName = $(this).data("for"); 
     $("#" + idName).addClass("red-border");
-    var element = document.getElementById(idName);
-    element.scrollIntoView({ behavior: 'smooth', block: 'center',inline:'center'});
+    element = document.getElementById(idName);
+    element.scrollIntoView({behavior: 'smooth', block: 'center',inline:'center'});
   });
 
   $("input").focusout(function() {
-    var idName = $(this).data("for"); 
+    idName = $(this).data("for"); 
     $("#" + idName).removeClass("red-border");
   });
   
 });
 
 
-
-
-
-
-// const element = document.getElementById("blabla");
-//   element.scrollIntoView();
