d68505098da03928c8970f5023477f92661582ca
Sosovi <nhokpio95@gmail.com>
2022-03-27 00:32:44 +0700

    input demo js

--- assets/js/tsd_script.js
+++ assets/js/tsd_script.js
@@ -1,8 +1,34 @@
 
 
-
 $(function(){
 
   $("#previewForm").load("./template-1.html"); 
   
+  $("input").keyup(function() {
+    var idName = $(this).data("for"); 
+    var value = $(this).val();
+    $("#" + idName).html(value);
+  });
+  
+
+  $("input").focus(function() {
+    var idName = $(this).data("for"); 
+    $("#" + idName).addClass("red-border");
+    var element = document.getElementById(idName);
+    element.scrollIntoView({ behavior: 'smooth', block: 'center',inline:'center'});
+  });
+
+  $("input").focusout(function() {
+    var idName = $(this).data("for"); 
+    $("#" + idName).removeClass("red-border");
+  });
+  
 });
+
+
+
+
+
+
+// const element = document.getElementById("blabla");
+//   element.scrollIntoView();
